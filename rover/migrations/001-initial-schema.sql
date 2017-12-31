653efde5954d4a386efc4983d01bd8d8a48d350c
Adam Pacholski <apachols@gmail.com>
2017-12-30 18:27:07 -0800

    Since this is deployed lets scrub that test data :)

--- rover/migrations/001-initial-schema.sql
+++ rover/migrations/001-initial-schema.sql
@@ -6,8 +6,8 @@ create table user (
   created INTEGER
 );
 
-insert into user values (1,'lindea85@gmail.com','206-551-2060', 1514677744);
-insert into user values (2,'lindea85@gmail.com','206-551-2060', 1514677744);
+insert into user values (1,'test1@example.com','+19005551212', 1514677744);
+insert into user values (2,'test2@example.com','+19005551234', 1514687173);
 
 create table owner (
   ownerid INTEGER primary key,
